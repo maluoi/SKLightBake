09599291e36b6cd62a35667eb203d6a11899aa89
Nick Klingensmith <programmerpichu@gmail.com>
2022-10-01 18:33:05 -0700

    Initial code

---
+++ Program.cs
@@ -0,0 +1,100 @@
+using StereoKit;
+using StereoKit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace SKLightBake;
+
+class Program
+{
+	static StaticScene staticScene;
+	static BakedScene  bakedScene;
+	static float       bakedSamples = 16;
+
+	static Mesh     floorMesh;
+	static Material floorMaterial;
+	static Mesh     cubeMesh;
+	static Material cubeMaterial;
+
+	static void Main(string[] args)
+	{
+		// Initialize StereoKit
+		SKSettings settings = new SKSettings
+		{
+			appName      = "SKLightBake",
+			assetsFolder = "Assets",
+		};
+		if (!SK.Initialize(settings))
+			Environment.Exit(1);
+
+		floorMesh   = Mesh.GeneratePlane(V.XY(10, 10), 20);
+		cubeMesh    = Mesh.GenerateCube (Vec3.One, 8);
+		floorMaterial = Material.Default.Copy();
+		floorMaterial[MatParamName.DiffuseTex] = Tex.FromFile("test.png");
+		cubeMaterial = Material.Default.Copy();
+		cubeMaterial[MatParamName.DiffuseTex] = Tex.FromFile("floor.png");
+
+		staticScene = GenerateScene();
+
+		bakedScene  = new BakedScene();
+		bakedScene.AddDirectionalLight(V.XYZ(-1, -1, -1), Color.White, 1);
+		bakedScene.SetSky(Renderer.SkyLight);
+		bakedScene.Bake(staticScene, 0);
+		Task.Run(() => bakedScene.Bake(staticScene, (int)bakedSamples));
+
+		// Core application loop
+		while (SK.Step(() =>
+		{
+			bakedScene.Draw();
+			WindowBakeSettings();
+		}));
+		SK.Shutdown();
+	}
+
+	static StaticScene GenerateScene()
+	{
+		float height = -1.6f;
+		if (World.HasBounds)
+			height = World.BoundsPose.position.y;
+
+		Random      r      = new Random();
+		StaticScene result = new StaticScene();
+		result.AddMesh(floorMesh, floorMaterial, Matrix.T(0,height,0));
+		for (int i = 0; i < 20; i++)
+		{
+			Vec3 at    = new Vec3(r.NextSingle() * 10 - 5, r.NextSingle() * 4 + height, r.NextSingle() * 10 - 5);
+			Vec3 scale = new Vec3(r.NextSingle() * 3 + 0.5f, r.NextSingle() * 3 + 0.5f, r.NextSingle() * 3 + 0.5f);
+			result.AddMesh(cubeMesh, cubeMaterial, Matrix.TS(
+				at + V.XYZ(0,scale.y/2.0f, 0), scale ));
+		}
+
+		return result;
+	}
+
+	static Pose bakeSettingsPose = new Pose(0, 0, -0.5f, Quat.LookDir(0, 0, 1));
+	static void WindowBakeSettings()
+	{
+		UI.WindowBegin("Bake Settings", ref bakeSettingsPose);
+		UI.Label("Samples"); UI.SameLine(); UI.HSlider("Samples", ref bakedSamples, 0, 512, 1);
+
+		UI.PushEnabled(!bakedScene.Baking);
+		if (UI.Button("Regenerate"))
+		{
+			staticScene = GenerateScene();
+			bakedScene.Bake(staticScene, 0);
+		}
+
+		UI.SameLine();
+		if (UI.Button("Bake"))
+			Task.Run(() => bakedScene.Bake(staticScene, (int)bakedSamples));
+
+		UI.PopEnabled();
+		if (bakedScene.Baking)
+		{
+			UI.SameLine();
+			UI.Label("Baking...");
+			UI.ProgressBar(bakedScene.BakingProgress);
+		}
+		UI.WindowEnd();
+	}
+}
---
+++ StaticScene.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace StereoKit.Framework
+{
+	public class StaticScene
+	{
+		internal List<StaticSceneItem> _items = new List<StaticSceneItem>();
+
+		public void AddModel(Model model, Matrix at)
+		{
+			if (model == null) return;
+
+			foreach (ModelNode node in model.Nodes)
+			{
+				if (node.Mesh == null) continue;
+				
+				string name = node.Name;
+				AddMesh(node.Mesh, node.Material, node.ModelTransform * at,
+					name.Contains("[invisible]" ) == false,
+					name.Contains("[intangible]") == false);
+			}
+		}
+
+		public void AddMesh(Mesh mesh, Material material, Matrix at, bool solid = true, bool visible = true)
+		{
+			StaticSceneItem item = new StaticSceneItem();
+			item.visible      = visible;
+			item.solid        = solid;
+			item.material     = material;
+			item.mesh         = mesh;
+			item.transform    = at;
+			item.invTransform = at.Inverse;
+			_items.Add(item);
+		}
+
+		public bool Raycast(Ray worldRay, out Ray at)
+		{
+			float closest = float.MaxValue;
+			at = default;
+			for (int i = 0; i < _items.Count; i++)
+			{
+				if (_items[i].Raycast(worldRay, out Ray curr))
+				{
+					float dist = Vec3.DistanceSq(curr.position, worldRay.position);
+					if (dist < closest)
+					{
+						closest = dist;
+						at      = curr;
+					}
+				}
+			}
+			return closest != float.MaxValue;
+		}
+
+		public void Draw()
+		{
+			for (int i = 0; i < _items.Count; i++)
+				_items[i].Draw();
+		}
+	}
+
+	struct StaticSceneItem
+	{
+		internal Matrix   transform;
+		internal Matrix   invTransform;
+		internal Mesh     mesh;
+		internal Material material;
+		internal bool     solid;
+		internal bool     visible;
+
+		public bool Raycast(Ray worldRay, out Ray intersection)
+		{
+			if (solid)
+			{
+				bool result = mesh.Intersect(invTransform.Transform(worldRay), out intersection);
+				if (result) intersection = transform.Transform(intersection);
+				return result;
+			}
+			else
+			{
+				intersection = default;
+				return false;
+			}
+		}
+
+		public void Draw() { if (visible) mesh.Draw(material, transform); }
+	}
+}
